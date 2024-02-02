06b6b42dae1ca1cbafaa4734230583250a6809f6
wipesides <atasesli@protonmail.com>
2024-02-02 15:30:50 +0300

    News part integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { NewsComponent } from './news/news.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NewsComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
