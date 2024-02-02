c274272efb839ba6ef1daf38704a5798fa0f42ac
wipesides <atasesli@protonmail.com>
2024-02-02 18:21:15 +0300

    Added sample posts and news

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NewsComponent } from './news/news.component';
+import { FeedComponent } from './feed/feed.component';
+import { HeaderComponent } from './header/header.component';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
     NewsComponent,
+    FeedComponent,
+    HeaderComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
