cba9288933deafe38a6aa9ff9437d3383fb7e51e
wipesides <atasesli@protonmail.com>
2024-02-05 13:51:37 +0300

    commit

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -1,17 +1,18 @@
 import { Component,inject,OnInit } from '@angular/core';
 import { PostsService } from '../posts.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-feed',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './feed.component.html',
   styleUrl: './feed.component.css'
 })
 export class FeedComponent implements OnInit{
-    posts = {};
-    ngOnInit(): void {
+  posts : any[] = [];
+   async ngOnInit(){
       try {
-        this.posts = this.postsService.getPosts();
+        this.posts = await this.postsService.getPosts();
       } catch (error) {
         console.error('Failed to fetch posts:',error)
       }
