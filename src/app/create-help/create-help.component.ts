441fa80c1202f1367984ea8304cdd6f075b15650
wipesides <atasesli@protonmail.com>
2024-02-06 17:26:11 +0300

    Created components inside the header

---
+++ src/app/create-help/create-help.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-help',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-help.component.html',
+  styleUrl: './create-help.component.css'
+})
+export class CreateHelpComponent {
+
+}
