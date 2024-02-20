7bbd86e769ff678c1ebf274e6c48c64dfdac5570
wipesides <atasesli@protonmail.com>
2024-02-20 03:26:24 +0300

    Applied feed changes to explore as well

--- src/app/explore/explore.component.ts
+++ src/app/explore/explore.component.ts
@@ -16,6 +16,8 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
   styleUrl: './explore.component.css'
 })
 export class ExploreComponent implements OnInit{
+  userData: any;
+  bookmarks: any[] = [];
   filteredPosts: any[] = [];
   allPosts: any[] = [];
   searchForm = new FormGroup({
@@ -23,11 +25,33 @@ export class ExploreComponent implements OnInit{
   })
   constructor(private service: FirebaseService){}
   async ngOnInit(){
-    this.allPosts = await this.service.getPosts();
+    this.allPosts = (await this.service.getPosts()).reverse();
+    this.userData = await this.service.getUserData();
+    this.bookmarks = await this.service.getUserBookmarks(this.userData);
   }
   filterPosts(){
     this.filteredPosts = this.allPosts.filter(post => {
       post.postTitle.includes(this.searchForm.value.search) || post.postBody.includes(this.searchForm.value.search)
     });
+  }
+  isBookmarked(post: any): boolean{
+    return this.bookmarks.includes(post.id)
+  }
+  helpClicked(post: any){
+    this.service._helpPost(post);
+  }
+  questionsClicked(post: any){
+    // Implementation later
+  }
+  bookmarkClicked(post: any){
+    if (this.isBookmarked(post)){
+      this.service._unbookmarkPost(post);
+    }
+    else {
+      this.service._bookmarkPost(post);
+    }
+  }
+  optionsClicked(post: any){
+    // Implementation later
   }
 }
